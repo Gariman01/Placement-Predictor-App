5230d22ddb84447cb806d38cbf4bf28769052fab
Gariman Singh <sgarimann@gmail.com>
2024-05-19 17:44:54 +0530

    Version 1

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import requests
+import time
 
 # Define the Flask API endpoint
 API_ENDPOINT = "https://placement-predictor-adt9.onrender.com/predict"
 
 # Set the page configuration
-st.set_page_config(page_title = "Campus Placement Prediction", layout = "wide")
+st.set_page_config(page_title="Campus Placement Prediction", layout="wide")
 
 # Function to submit the form data to the Flask API
 def get_prediction(data):
@@ -13,31 +14,28 @@ def get_prediction(data):
     return response.json()
 
 # Title and description
-st.title("Campus Placement Prediction")
-st.markdown("Enter Details for forecast.")
+st.title("Will you be placed at Plaksha?")
+st.markdown("Enter your details below to predict!")
 
 # Form
 with st.form(key='placement_form'):
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        ssc_p = st.number_input("SSC PR%: [45-100]", min_value=45, max_value=100, step=1)
-        hsc_p = st.number_input("HSC PR%: [45-100]", min_value=45, max_value=100, step=1)
-        degree_p = st.number_input("Degree PR%: [35-100]", min_value=35, max_value=100, step=1)
-        etest_p = st.number_input("etest_p: [0-100]", min_value=0, max_value=100, step=1)
+        mba_p = st.number_input("Plaksha TLF CGPA: [4-10]", min_value=4., max_value=10., step=0.1) * 10
+        workex = st.selectbox("Work Experience", options=["No", "Yes"], index=0)
+        degree_t = st.selectbox("UG Degree Type", options=["Comm&Mgmt", "Others", "Sci&Tech"], index=0)
+        degree_p = st.number_input("UG Degree CPI: [4-10]", min_value=4., max_value=10., step=0.1) * 10
 
     with col2:
-        mba_p = st.number_input("mba_p: [0-100]", min_value=0, max_value=100, step=1)
-        gender = st.selectbox("Gender", options=["Male", "Female"], index=0)
-        ssc_b = st.selectbox("SSC Board", options=["Central", "Others"], index=0)
-        hsc_b = st.selectbox("HSC Board", options=["Central", "Others"], index=0)
+        hsc_s = st.selectbox("Class 12th Stream", options=["Arts", "Commerce", "Science"], index=0)
+        hsc_b = st.selectbox("Class 12th Board", options=["Central", "Others"], index=0)
+        hsc_p = st.number_input("Class 12th %age: [40-100]", min_value=40., max_value=100., step=1.)
 
     with col3:
-        hsc_s = st.selectbox("HSC Stream", options=["Arts", "Commerce", "Science"], index=0)
-        degree_t = st.selectbox("Degree Type", options=["Comm&Mgmt", "Others", "Sci&Tech"], index=0)
-        workex = st.selectbox("Work Experience", options=["No", "Yes"], index=0)
-        specialisation = st.selectbox("Specialisation", options=["Mkt&Fin", "Mkt&HR"], index=0)
-
+        gender = st.selectbox("Gender", options=["Male", "Female"], index=0)
+        ssc_b = st.selectbox("Class 10th Board", options=["Central", "Others"], index=0)
+        ssc_p = st.number_input("Class 10th %age: [40-100]", min_value=40., max_value=100., step=1.)
     submit_button = st.form_submit_button(label='Predict')
 
 # Convert form inputs to appropriate format
@@ -46,7 +44,6 @@ if submit_button:
         "ssc_p": ssc_p,
         "hsc_p": hsc_p,
         "degree_p": degree_p,
-        "etest_p": etest_p,
         "mba_p": mba_p,
         "gender": 0 if gender == "Male" else 1,
         "ssc_b": 0 if ssc_b == "Central" else 1,
@@ -54,16 +51,19 @@ if submit_button:
         "hsc_s": {"Arts": 0, "Commerce": 1, "Science": 2}[hsc_s],
         "degree_t": {"Comm&Mgmt": 0, "Others": 1, "Sci&Tech": 2}[degree_t],
         "workex": 0 if workex == "No" else 1,
-        "specialisation": 0 if specialisation == "Mkt&Fin" else 1
     }
+
+    # Clear previous prediction
+    st.header("Prediction Result")
+    prediction_placeholder = st.empty()
+    time.sleep(0.5)  # Add a small delay for the vanishing effect
 
     # Call the API to get the prediction
     prediction = get_prediction(data)
     prediction_text = prediction.get('prediction', 'No result')
 
-    # Display the result
-    st.header("Prediction Result")
-    st.markdown(f"**Student will be:** {prediction_text}")
+    # Display the new result
+    prediction_placeholder.markdown(f"**Thou shall be:** {prediction_text}")
 
 # Footer with project details
 st.markdown("""
@@ -72,10 +72,14 @@ st.markdown("""
     <p>Project by</p>
     <h2>Gariman Singh</h2>
     <p>
-	MS Physics & Data Science, IISER Mohali <br>
-	Plaksha Tech Leaders Fellowship <br>
-	<a href = "https://www.linkedin.com/in/gariman-singh-643b9149/"> LinkedIn </a>
+        MS Physics & Data Science, IISER Mohali <br>
+        Plaksha Tech Leaders Fellowship <br>
+        <a href="https://github.com/Gariman01" target="_blank">
+            GitHub
+        </a>   &nbsp;
+        <a href="https://www.linkedin.com/in/gariman-singh-643b9149/" target="_blank">
+            Linkedin
+        </a>
     </p>
-
 </div>
 """, unsafe_allow_html=True)
